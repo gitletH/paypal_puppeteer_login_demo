3d2ad36f2c6ab985c79544862e4cc2122e48487e
Tianjiao Huang <maitian519@gmail.com>
2019-05-12 02:02:58 -0700

    more logs

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ const puppeteer = require('puppeteer')
 
 const email = "";
 const password = "";
+// You might want to change this if this is invalid when you test it
+const login_url = "https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-4JR04850Y9063343G"
 
 const login_png = 'paypal_login_page.png';
 const loading_png = 'paypal_loading.png';
 const payment_png = 'paypal_payment.png';
 (async () => {
+  console.log("Launching headless chrome")
   const browser = await puppeteer.launch({headless: true})
+  console.log("Opening a new page")
   const page = await browser.newPage()
-  await page.goto('https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token=EC-4JR04850Y9063343G') // You might want to change this if this is invalid when you test it
+  console.log("Redirect to " + login_url)
+  await page.goto(login_url) // You might want to change this if this is invalid when you test it
   await page.screenshot({ path: login_png })
   await page.type('#email', email)
   await page.type('#password', password)
   await page.click('[name="btnLogin"]')
+  console.log("Logging in as " + email)
   await page.waitForNavigation()
   await page.screenshot({ path: loading_png })
+  console.log("Logged in ")
   await page.waitForNavigation()
   await page.screenshot({ path: payment_png })
   browser.close()
